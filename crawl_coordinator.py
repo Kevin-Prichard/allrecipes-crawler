ce86480b6f8d7192e7b8d6c74e2a94b02ad6aed5
kev <kprichard@gmail.com>
2021-08-02 22:06:10 -0700

    Works- checking URI existence and fetching recipes

--- crawl_coordinator.py
+++ crawl_coordinator.py
@@ -1,4 +1,5 @@
 import multiprocessing.dummy as mp
+import random
 import re
 import requests
 import sys
@@ -8,8 +9,8 @@ from threading import Thread, Lock
 mutex = Lock()
 
 from datastore import RecipeStore
-
-# from recipe_scrapers import scrape_me
+import json
+from recipe_scrapers import scrape_me
 from recipe_scrapers._abstract import AbstractScraper
 
 
@@ -24,17 +25,7 @@ class CrawlCoordinator:
         self.store = RecipeStore()
 
     def start(self):
-        # import pudb; pu.db
-        url_gen = self._start_discovery() <- isn\'t a generator
-        try:
-            while True:
-                print("start...")
-                url = next(url_gen)  <- NoneType is not an iterator
-                print("CrawlCoordinator.start: " + url)
-        except StopIteration:
-            import pudb; pu.db
-            print("Finished URL discovery")
-            return
+        self._start_discovery()
 
     def _start_discovery(self):
         # site_explorers = [RecipeSiteUrlIterator(klass)
@@ -43,7 +34,8 @@ class CrawlCoordinator:
         urls_counted = 0
 
         with mp.Pool(4*len(self.scraper_classes)) as dpool:
-            RECIPE_ID_REGEX = re.compile(r".*[^/]+/(\d+)$")
+            RECIPE_ID_REGEX = re.compile(r".*/recipe/(\d+)/[a-z0-9_\-.]+/?$",
+                                         re.I)
             found_ids = []
             site_iterator = dpool.imap_unordered(
                 CrawlCoordinator.discovery_runner,
@@ -51,21 +43,30 @@ class CrawlCoordinator:
 
             try:
                 while True:
-                    # print("_start_discovery ...1")
+                    print("_start_discovery ...1")
                     item = next(site_iterator)
+                    print("_start_discovery ...1 ", str(item))
                     while True:
                         # print("_start_discovery ...2")
-                        an_item = next(item)
-                        recipe_id = RECIPE_ID_REGEX.match(an_item).groups()[0]
+                        a_recipe_uri = next(item)
+                        if not a_recipe_uri:
+                            continue
+                        recipe_id = RECIPE_ID_REGEX.match(a_recipe_uri).groups()[0]
                         mutex.acquire()
                         try:
+                            recipe = scrape_me(a_recipe_uri)
+                            # r = requests.get(a_recipe_uri)
+                            # if r.ok:
+                            #     print(f"{a_recipe_uri} - {len(r.text)}")
+                            recipe_dumpus = json.dumps(recipe.to_dict())
+                            print(f"{a_recipe_uri} - {recipe_dumpus}\n\n")
                             found_ids.append(int(recipe_id))
                             urls_counted += 1
                             # print("_start_discovery ...3 " +
                             #       str(an_item) + "    " + str(urls_counted))
                             if urls_counted / 100 == int(urls_counted / 100):
-                                sys.stderr.write(
-                                    "urls_counted: " + str(urls_counted) + "\n")
+                                # sys.stderr.write(
+                                print("urls_counted: " + str(urls_counted))
                         finally:
                             mutex.release()
             except StopIteration as ee:
@@ -82,10 +83,17 @@ class CrawlCoordinator:
 
     @classmethod
     def discovery_runner(cls, scraper_class):
-        # import pudb; pu.db
+        def choose(x):
+            return True
+            # yes = random.random() > 0.6666
+            # return yes
+
+        yield from scraper_class.site_iterator(choose, 1, 10000)
+
         # print("discovery_runner startup")
-        site_iter = scraper_class.site_iterator(None, 1, 10000)
+        # site_iter = scraper_class.site_iterator(None, 1, 10000)
         # print("discovery_runner site_iter: "+str(site_iter))
+        """
         try:
             while True:
                 # print("d-r...")
@@ -95,3 +103,4 @@ class CrawlCoordinator:
         except StopIteration:
             print("discovery_runner StopIteration")
             return
+        """
